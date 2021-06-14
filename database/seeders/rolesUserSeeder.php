4cbb78d536806f10861f4f45a66bdde43efeeeb3
kanivip1 <kanivip284@gmail.com>
2021-06-14 14:53:11 +0700

    add to roles

---
+++ database/seeders/rolesUserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class rolesUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(DB::table('roles')->count() == 0){
+
+            DB::table('roles')->insert([
+                [
+                    'name' => 'admin',
+                    'content' => 'người quản trị',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'user',
+                    'content' => 'người dùng',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+            ]);
+            
+        } else { echo "Table have data"; }
+    }
+}
