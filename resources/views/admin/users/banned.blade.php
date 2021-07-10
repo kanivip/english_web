32733284457e8b8e60f776c6fb26b432cc247e10
Harley <harley040521@gmail.com>
2021-07-10 14:43:35 +0700

    no message

--- resources/views/admin/users/banned.blade.php
+++ resources/views/admin/users/banned.blade.php
@@ -20,7 +20,7 @@
         <div class="ecommerce-widget">
 
 
-            <!-- basic   -->
+            <!-- basic form  -->
             <!-- ============================================================== -->
             <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
